394a4484a30b949df98d30aa6e24f9532fed0272
Quantumminded <lukkino31@gmail.com>
2022-11-27 17:54:00 +0100

    card section done

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
 import Image from 'next/image';
 import deved from '../public/dev-ed-wave.png';
+import design from '../public/design.png';
+import code from '../public/code.png';
+import consulting from '../public/consulting.png';
 
 export default function Home() {
   return (
@@ -51,6 +54,53 @@ export default function Home() {
               for both business and consumers use. 
             </p>
             <p className='text-md py-2 leading-8 text-gray-800'>I offer from a wide range of services, including brand design and programming.</p>        
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+                <Image src={design} width={100} height={100} />
+                <h3 className="text-lg font-medium pt-8 pb-2  ">
+                  Beautiful Designs
+                </h3>
+                <p className="py-2">
+                  Creating elegant designs suited for your needs following core
+                  design theory.
+                </p>
+                <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+                <p className="text-gray-800 py-1">Photoshop</p>
+                <p className="text-gray-800 py-1">Illustrator</p>
+                <p className="text-gray-800 py-1">Figma</p>
+                <p className="text-gray-800 py-1">Indesign</p>
+              </div>
+              <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+                <Image src={code} width={100} height={100} />
+                <h3 className="text-lg font-medium pt-8 pb-2 ">
+                  Code your dream project
+                </h3>
+                <p className="py-2">
+                  Do you have an idea for your next great website? Let's make it a
+                  reality.
+                </p>
+                <h4 className="py-4 text-teal-600">Coding Tools I Use</h4>
+                <p className="text-gray-800 py-1">Html</p>
+                <p className="text-gray-800 py-1">Css</p>
+                <p className="text-gray-800 py-1">JS</p>
+                <p className="text-gray-800 py-1">React</p>
+                <p className="text-gray-800 py-1">Node</p>
+                <p className="text-gray-800 py-1">Express</p>
+              </div>
+              <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+                <Image src={consulting} width={100} height={100} />
+                <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
+                <p className="py-2">
+                  Are you interested in feedback for your current project? I can
+                  give you tips and tricks to level it up.
+                </p>
+                <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
+                <p className="text-gray-800 py-1">Photoshop</p>
+                <p className="text-gray-800 py-1">Illustrator</p>
+                <p className="text-gray-800 py-1">Figma</p>
+                <p className="text-gray-800 py-1">Indesign</p>
+              </div>
           </div>
         </section>
       </main>
