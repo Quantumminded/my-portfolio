dbc108704d0726c94628392d70ddfda679290fc0
Quantumminded <lukkino31@gmail.com>
2022-11-27 15:38:16 +0100

    modify tailwind.config

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ export default function Home() {
       </Head>
 
       <main>
+        <section className=" bg-blue-700 min-h-screen">
+          <h1>Hello im Luca</h1>
+        </section>
       </main>
     </div>
   )
