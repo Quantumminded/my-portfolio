d15d6fac505d3e86675559e24cb1514efe2ce069
Quantumminded <lukkino31@gmail.com>
2022-11-27 16:58:42 +0100

    adding skills section

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,18 @@ export default function Home() {
             <Image src={deved} layout='fill' objectFit='cover' />
           </div>
         </section>
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Skills</h3>
+            <p className='text-md py-2 leading-8 text-gray-800'>
+              Since the beginning of my journey as a Web & App developer, i've done remote work for
+              <span className='text-teal-500'> agencies </span>  consulted for ,<span className='text-teal-500'> startups </span>
+              and collaborated with talented people to create digital products
+              for both business and consumers use. 
+            </p>
+            <p className='text-md py-2 leading-8 text-gray-800'>I offer from a wide range of services, including brand design and programming.</p>        
+          </div>
+        </section>
       </main>
     </div>
   )
