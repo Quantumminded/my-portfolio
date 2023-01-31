4154e1a02432f3578d902bd499002ad06488fcd0
Quantumminded <lukkino31@gmail.com>
2023-01-31 22:46:38 +0100

    adding git link to portfolio component

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -80,7 +80,7 @@ const Portfolio = () => {
         </div>
 
         <div className="flex items-center justify-center">
-          <Link href="/portfolio">
+          <Link href="https://github.com/Quantumminded">
             <div className="group flex items-center justify-center my-8 bg-blue-500 dark:bg-teal-400 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
               all projects
               <span className="-rotate-90 duration-100 ease-in group-hover:translate-x-5">
