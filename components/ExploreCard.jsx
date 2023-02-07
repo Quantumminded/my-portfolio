1660aa1c9a5b18a23498d07cd954c940461139ff
Quantumminded <lukkino31@gmail.com>
2023-02-07 22:38:45 +0100

    fixing bg color of card desciption

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -18,58 +18,118 @@ const ExploreCard = ({
   git,
   description,
 }) => (
-  <motion.div
-    variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-    className={`relative ${
-      active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
-    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
-    onClick={() => handleClick(id)}
-  >
-    <img
-      src={imgUrl}
-      alt={title}
-      className="absolute w-full h-full object-cover rounded-[24px]"
-    />
-    {active !== id ? (
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
-        {title}
-      </h3>
-    ) : (
-      <div className="absolute bottom-0 p-4 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] rounded-xl xs:p-3 md:p-6">
-        <div
-          className={`flex justify-items-center w-[65px] h-[65px] rounded-[24px] glassmorphism mb-[-35px] gap-8`}
-        >
-          {stack.map((item, index) => (
-            <img
-              key={index}
-              src={item}
-              alt={title}
-              className="w-1/2 h-1/2 object-contain xs:pb-2"
-            />
-          ))}
-        </div>
-        <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase xs:text-[7px] sm:text-[13px]  ">
-          {description}
-        </p>
-        <div className=" flex items-center justify-center align-middle text-sm text-gray-600 gap-8 p-3">
-          <div className="flex items-center justify-center rounded-full dark:bg-teal-400 bg-blue-600  hover:scale-105 duration-200 p-3 cursor-pointer">
-            <a href={git} target="_blank">
-              <FaGithub size={25} className="fill-white dark:fill-black"/>
-            </a>
-          </div>
+  // <motion.div
+  //   variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+  //   className={`relative ${
+  //     active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
+  //   } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+  //   onClick={() => handleClick(id)}
+  // >
+  //   <img
+  //     src={imgUrl}
+  //     alt={title}
+  //     className="absolute w-full h-full object-cover rounded-[24px]"
+  //   />
+  //   {active !== id ? (
+  //     <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
+  //       {title}
+  //     </h3>
+  //   ) : (
+  //     <div className="absolute bottom-0 p-4 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.85)] rounded-b-[24px] rounded-xl xs:p-3 md:p-6">
+  //       <div
+  //         className={`flex justify-items-center w-[65px] h-[65px] rounded-[24px] glassmorphism mb-[-35px] gap-8`}
+  //       >
+  //         {stack.map((item, index) => (
+  //           <img
+  //             key={index}
+  //             src={item}
+  //             alt={title}
+  //             className="w-1/2 h-1/2 object-contain xs:pb-2"
+  //           />
+  //         ))}
+  //       </div>
+  //       <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase xs:text-[10px] sm:text-[13px]  ">
+  //         {description}
+  //       </p>
+  //       <div className=" flex items-center justify-center align-middle text-sm text-gray-600 gap-8 p-3">
+  //         <div className="flex items-center justify-center rounded-full dark:bg-teal-400 bg-blue-600  hover:scale-105 duration-200 p-3 cursor-pointer">
+  //           <a href={git} target="_blank">
+  //             <FaGithub size={25} className="fill-white dark:fill-black"/>
+  //           </a>
+  //         </div>
 
-          <div className="flex items-center justify-center rounded-full dark:bg-teal-400 bg-blue-600 hover:scale-105 duration-200 p-3 cursor-pointer">
-            <a href={url} target="_blank">
-              <FiExternalLink size={25} className="bg-white dark:bg-transparent rounded-full p-1" />
-            </a>
-          </div>
-        </div>
-        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
-          {title}
-        </h2>
+  //         <div className="flex items-center justify-center rounded-full dark:bg-teal-400 bg-blue-600 hover:scale-105 duration-200 p-3 cursor-pointer">
+  //           <a href={url} target="_blank">
+  //             <FiExternalLink size={25} className="bg-white dark:bg-transparent rounded-full p-1" />
+  //           </a>
+  //         </div>
+  //       </div>
+  //       <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+  //         {title}
+  //       </h2>
+  //     </div>
+  //   )}
+  // </motion.div>
+     
+  <motion.div
+  variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+  className={`relative ${
+    active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
+  } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+  onClick={() => handleClick(id)}
+>
+  <img
+    src={imgUrl}
+    alt={title}
+    className="absolute w-full h-full object-cover rounded-[24px]"
+  />
+  {active !== id ? (
+    <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
+      {title}
+    </h3>
+  ) : (
+    <div className="absolute bottom-0 p-4 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.85)] rounded-b-[24px] rounded-xl xs:p-3 md:p-6">
+      <h2 className=" font-semibold sm:text-[32px] text-[24px] text-white text-center">
+        {title}
+      </h2>
+      <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase xs:text-[10px] sm:text-[13px] mb-3 text-center">
+        {description}
+      </p>
+      
+      <div
+        className={`flex justify-items-center  w-[65px] h-[65px] rounded-[24px] glassmorphism mb-[-35px] gap-8 `}
+      >
+        
+        {stack.map((item, index) => (
+          <img
+            key={index}
+            src={item}
+            alt={title}
+            className="w-1/2 h-1/2 object-contain xs:pb-2"
+          />
+        ))}
+        
       </div>
-    )}
-  </motion.div>
+
+      <div className=" flex items-center justify-center align-middle text-sm text-gray-600 gap-8 p-3">
+        <div className="flex items-center justify-center rounded-full dark:bg-teal-400 bg-blue-600  hover:scale-105 duration-200 p-3 cursor-pointer">
+          <a href={git} target="_blank">
+            <FaGithub size={25} className="fill-white dark:fill-black"/>
+          </a>
+        </div>
+
+        <div className="flex items-center justify-center rounded-full dark:bg-teal-400 bg-blue-600 hover:scale-105 duration-200 p-3 cursor-pointer">
+          <a href={url} target="_blank">
+            <FiExternalLink size={25} className="bg-white dark:bg-transparent rounded-full p-1" />
+          </a>
+        </div>
+      </div>
+
+    </div>
+  )}
+</motion.div>
+
+
 );
 
 export default ExploreCard;
