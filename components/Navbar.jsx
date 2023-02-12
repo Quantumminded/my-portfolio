7368af9e60adde9ec1dcc7a3e42ea6775d90f563
Quantumminded <lukkino31@gmail.com>
2023-02-12 18:33:32 +0100

    adding link to burger menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,6 @@ import logo from "../public/Logo.png";
 import luca from "../public/luca.png";
 import Image from "next/image";
 
-
 import {
   FaBars,
   FaTimes,
@@ -16,11 +15,9 @@ import {
   FaLinkedin,
 } from "react-icons/fa";
 
-const Navbar = ({darkMode, setDarkMode}) => {
+const Navbar = ({ darkMode, setDarkMode }) => {
   const [navigation, setNavigation] = useState(false);
   const [pageScroll, setPageScroll] = useState(false);
-  
-  
 
   useEffect(() => {
     const sub = window.addEventListener("scroll", () =>
@@ -55,109 +52,116 @@ const Navbar = ({darkMode, setDarkMode}) => {
 
   return (
     <nav className="flex dark:text-white">
-        <div
-          className={`w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in ${
-            pageScroll && "bg-black text-[#fff]"
-          }`}
-        >
-          <div className="flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-3">
-            <Image src={logo} height="80px" width="80px" objectFit="" />
+      <div
+        className={`w-full h-20 z-10 fixed bg-white text-black duration-300 ease-in ${
+          pageScroll && "bg-black text-[#fff]"
+        }`}
+      >
+        <div className="flex justify-between items-center w-full h-full max-w-screen-xl mx-auto p-3">
+          <Image src={logo} height="80px" width="80px" objectFit="" />
 
+          <div>
+            <ul className="hidden md:flex">
+              {links.map(({ id, link }) => (
+                <Link key={id} href={`/#${link}`}>
+                  <li className="ml-10 text-sm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider">
+                    {link}
+                  </li>
+                </Link>
+              ))}
+              <BsFillMoonStarsFill
+                onClick={() => setDarkMode(!darkMode)}
+                className=" cursor-pointer text-2xl ml-10"
+              />
+            </ul>
+
+            {!navigation && (
+              <div
+                className="md:hidden cursor-pointer"
+                onClick={() => setNavigation(true)}
+              >
+                <FaBars size={30} />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div
+          className={
+            navigation
+              ? "md:hidden fixed left-0 top-0 w-full h-full bg-black/70 backdrop-blur"
+              : ""
+          }
+        >
+          <div
+            className={
+              navigation
+                ? "fixed left-0 top-0 w-4/5 h-full bg-gradient-to-r from-blue-600 to-cyan-600 text-white p-10 ease-in duration-500"
+                : "fixed top-0 left-[-100%] p-10 h-full ease-in duration-500"
+            }
+          >
             <div>
-              <ul className="hidden md:flex">
+              <div className="flex w-full items-center justify-between">
+                <Link href="/#home">
+                  <h2
+                    onClick={() => setNavigation(false)}
+                    className="text-3xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer"
+                  >
+                    Luca Stringhetti
+                  </h2>
+                </Link>
+                <div
+                  onClick={() => setNavigation(false)}
+                  className="p-3 cursor-pointer"
+                >
+                  <FaTimes size={30} />
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-24 flex flex-col h-fit gap-20">
+              <ul className="uppercase">
                 {links.map(({ id, link }) => (
                   <Link key={id} href={`/#${link}`}>
-                    <li className="ml-10 text-sm uppercase cursor-pointer duration-200 ease-out hover:scale-105 tracking-wider">
+                    <li
+                      onClick={() => setNavigation(false)}
+                      className="py-4 text-2xl tracking-wider cursor-pointer"
+                    >
                       {link}
                     </li>
                   </Link>
                 ))}
-                <BsFillMoonStarsFill
-                  onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl ml-10"
-                />
               </ul>
 
-              {!navigation && (
-                <div
-                  className="md:hidden cursor-pointer"
-                  onClick={() => setNavigation(true)}
-                >
-                  <FaBars size={30} />
-                </div>
-              )}
-            </div>
-          </div>
-
-          <div
-            className={
-              navigation
-                ? "md:hidden fixed left-0 top-0 w-full h-full bg-black/70 backdrop-blur"
-                : ""
-            }
-          >
-            <div
-              className={
-                navigation
-                  ? "fixed left-0 top-0 w-4/5 h-full bg-gradient-to-r from-blue-600 to-cyan-600 text-white p-10 ease-in duration-500"
-                  : "fixed top-0 left-[-100%] p-10 h-full ease-in duration-500"
-              }
-            >
               <div>
-                <div className="flex w-full items-center justify-between">
-                  <Link href="/#home">
-                    <h2
-                      onClick={() => setNavigation(false)}
-                      className="text-3xl font-bold uppercase underline underline-offset-2 tracking-wider cursor-pointer"
+                <div className="grid grid-cols-1 mx-auto w-4/5 gap-10">
+                  <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
+                    <a
+                      href="https://www.linkedin.com/in/lucastringhetti/"
+                      target="_blank"
                     >
-                      Luca Stringhetti
-                    </h2>
-                  </Link>
-                  <div
-                    onClick={() => setNavigation(false)}
-                    className="p-3 cursor-pointer"
-                  >
-                    <FaTimes size={30} />
-                  </div>
-                </div>
-              </div>
-
-              <div className="mt-24 flex flex-col h-fit gap-20">
-                <ul className="uppercase">
-                  {links.map(({ id, link }) => (
-                    <Link key={id} href={`/#${link}`}>
-                      <li
-                        onClick={() => setNavigation(false)}
-                        className="py-4 text-2xl tracking-wider cursor-pointer"
-                      >
-                        {link}
-                      </li>
-                    </Link>
-                  ))}
-                </ul>
-
-                <div>
-                  <div className="grid grid-cols-1 mx-auto w-4/5 gap-10">
-                    <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
                       <FaLinkedin size={25} />
-                    </div>
-                    <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
+                    </a>
+                  </div>
+                  <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
+                    <a href="https://github.com/Quantumminded" target="_blank">
                       <FaGithub size={25} />
-                    </div>
-                    <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
-                      <BsFillMoonStarsFill
-                        size={25}
-                        onClick={() => setDarkMode(!darkMode)}
-                        className=" cursor-pointer text-2xl"
-                      />
-                    </div>
+                    </a>
+                  </div>
+                  <div className="flex items-center justify-center rounded-full shadow-md shadow-white p-3 cursor-pointer">
+                    <BsFillMoonStarsFill
+                      size={25}
+                      onClick={() => setDarkMode(!darkMode)}
+                      className=" cursor-pointer text-2xl"
+                    />
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 
