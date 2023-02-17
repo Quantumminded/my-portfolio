336bef3dd77e9f7966fb1f23d3a083f13a34f32e
Quantumminded <lukkino31@gmail.com>
2023-02-17 01:37:09 +0100

    cleaning exloreCard

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -25,11 +25,7 @@ const ExploreCard = ({
   //   } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
   //   onClick={() => handleClick(id)}
   // >
-  //   <img
-  //     src={imgUrl}
-  //     alt={title}
-  //     className="absolute w-full h-full object-cover rounded-[24px]"
-  //   />
+  //   
   //   {active !== id ? (
   //     <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
   //       {title}
@@ -52,11 +48,7 @@ const ExploreCard = ({
   //         {description}
   //       </p>
   //       <div className=" flex items-center justify-center align-middle text-sm text-gray-600 gap-8 p-3">
-  //         <div className="flex items-center justify-center rounded-full dark:bg-teal-400 bg-blue-600  hover:scale-105 duration-200 p-3 cursor-pointer">
-  //           <a href={git} target="_blank">
-  //             <FaGithub size={25} className="fill-white dark:fill-black"/>
-  //           </a>
-  //         </div>
+  //         
 
   //         <div className="flex items-center justify-center rounded-full dark:bg-teal-400 bg-blue-600 hover:scale-105 duration-200 p-3 cursor-pointer">
   //           <a href={url} target="_blank">
